757d8ce49bff34df77aaefe954def48983d26683
hhhenrysss <25966196+hhhenrysss@users.noreply.github.com>
2020-02-03 18:26:09 -0600

    added widgets

--- code/components/statistics/components/summary_widget.tsx
+++ code/components/statistics/components/summary_widget.tsx
@@ -1,6 +1,8 @@
 import {StatsWidgetProps} from "../types/props";
 import * as React from "react";
 
+import '../../../styles/stats_summary_widget.scss';
+
 export class SummaryWidget extends React.PureComponent<StatsWidgetProps>{
     render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
         return (
@@ -8,7 +10,7 @@ export class SummaryWidget extends React.PureComponent<StatsWidgetProps>{
                 <h4 className={'widget_value'}>{this.props.value}</h4>
                 <div className={'widget_info_wrapper'}>
                     <h5 className={'widget_name'}>{this.props.name}</h5>
-                    <i className="material-icons widget_icon" onClick={() => this.props.click_callback(this.props.id)}>{this.props.type}</i>
+                    <i className="material-icons-outlined widget_icon" onClick={() => this.props.click_callback(this.props.id)}>{this.props.type}</i>
                 </div>
             </div>
         );
--- code/components/statistics/general.tsx
+++ code/components/statistics/general.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import {StatsProps} from "./types/props";
 import {ChartData, ChartType} from "./types/chart";
+import {SummaryWidget} from "./components/summary_widget";
 
 const charts: ChartData[] = [
     {id: 0, name: 'Most used programming language', first_item: 'JavaScript', data: null, type: ChartType.Bubble},
@@ -18,7 +19,72 @@ export class StatGeneral extends React.Component<StatsProps, {active_chart_idx: 
             active_chart_idx: -1
         };
     }
+    get active_chart_idx() {
+        return this.state.active_chart_idx;
+    }
+    set active_chart_idx(idx: number) {
+        const prev_state = this.state;
+        this.setState({...prev_state, active_chart_idx: idx});
+    }
+
+    get widgets() {
+        return (
+            <div className={'chart_list'}>
+                {
+                    charts.map(chart_item => {
+                        return <SummaryWidget
+                            key={chart_item.id}
+                            id={chart_item.id}
+                            name={chart_item.name}
+                            value={chart_item.first_item}
+                            type={chart_item.type}
+                            click_callback={i => this.active_chart_idx = i}
+                        />
+                    })
+                }
+            </div>
+        );
+    }
+    get headers() {
+        return (
+            <div>
+                <h1>{StatGeneral.statistics_name}</h1>
+                <h2>{StatGeneral.description}</h2>
+            </div>
+        );
+    }
+    get animation() {
+        return <div>Animation here</div>
+    }
+    get navigation() {
+        return (
+            <div>
+                <div onClick={() => this.props.update_idx_callback(this.props.left_info.idx)}>{this.props.left_info.name}</div>
+                <div onClick={() => this.props.update_idx_callback(this.props.right_info.idx)}>{this.props.right_info.name}</div>
+            </div>
+        );
+    }
+    get main_page() {
+        return (
+            <article>
+                <div>
+                    {this.headers}
+                    {this.animation}
+                </div>
+                {this.widgets}
+                {this.navigation}
+            </article>
+        );
+    }
+    get chart_page() {
+        // todo
+        return <h1>Hello</h1>;
+    }
     render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
-        return super.render();
+        if (this.active_chart_idx >= 0) {
+            return this.chart_page;
+        } else {
+            return this.main_page;
+        }
     }
 }
